ef0ed8abb75bc7d44a1ff19664dd619811d241b5
Garrett <Garrettdeblois13@yahoo.com>
2017-05-11 00:34:04 -0500

    Add project files.

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Move";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(42, 255);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(154, 147);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 474);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections; // ArrayList
+using System.IO; // streams
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        class Play // Move used by AI
+        {
+            int m;
+            string outcome;
+            public Play(int Position, string Outcome)
+            {
+                m = Position;
+                outcome = Outcome;
+            }
+            public int Position
+            {
+                get { return m; }
+            }
+            public string Outcome
+            {
+                get { return outcome; }
+            }
+            public override string ToString()
+            {
+                return m.ToString() + ", " + outcome;
+            }
+        }
+
+        string Game;
+        float X1, X2, X3, X4;
+        float Y1, Y2, Y3, Y4;
+        ArrayList Library;
+        ArrayList Moves;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Moves = new ArrayList();
+            this.Text = "Tic Tac Toe";
+            Game = "---------";
+            label1.Text = Game;
+            DrawGame(pictureBox1.Width);
+            // Load library
+            Library = new ArrayList();
+            StreamReader sr = new StreamReader("library.txt");
+            string Line = sr.ReadLine();
+            while (Line != null)
+            {
+                Library.Add(Line);
+                Line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+
+        void DrawGame(int Side)
+        {
+            Bitmap Display = new Bitmap(Side, Side);
+            Graphics g = Graphics.FromImage(Display);
+            SolidBrush BackgroundBrush = new SolidBrush(Color.White);
+            Pen GridPen = new Pen(Color.Black);
+            g.FillRectangle(BackgroundBrush, 0, 0, Display.Width, Display.Height);
+            X1 = 0.05f * Side;
+            X2 = 0.35f * Side;
+            X3 = 0.65f * Side;
+            X4 = 0.95f * Side;
+            Y1 = 0.05f * Side;
+            Y2 = 0.35f * Side;
+            Y3 = 0.65f * Side;
+            Y4 = 0.95f * Side;
+            g.DrawLine(GridPen, X2, Y1, X2, Y4);
+            g.DrawLine(GridPen, X3, Y1, X3, Y4);
+            g.DrawLine(GridPen, X1, Y2, X4, Y2);
+            g.DrawLine(GridPen, X1, Y3, X4, Y3);
+
+            for (int square = 0; square < 9; square++)
+            {
+                if (Game[square] != '-')
+                {
+                    // Compute row and column
+                    int r = square / 3;
+                    int c = square % 3;
+                    // Create point for upper-left corner of drawing.
+                    float x = X1 + c * (X3 - X2) + 10;
+                    float y = Y1 + r * (Y3 - Y2) + 10;
+                    String drawString = "";
+                    SolidBrush CBrush;
+                    if (Game[square] == 'X')
+                    {
+                        CBrush = new SolidBrush(Color.Purple);
+                        drawString = "X";
+                    }
+                    else // must be 'O'
+                    {
+                        CBrush = new SolidBrush(Color.Green);
+                        drawString = "O";
+                    }
+                    // Create font and brush
+                    Font drawFont = new Font("Arial", 16);
+                    // Set format of string
+                    StringFormat drawFormat = new StringFormat();
+                    // Draw string to screen
+                    g.DrawString(drawString, drawFont, CBrush, x, y, drawFormat);
+                } // end if
+            } // end for
+            pictureBox1.Image = Display;
+            // Check and see if game is over
+            if (WonGame(Game, 'X')) GameOver('X');
+            if (WonGame(Game, 'O')) GameOver('O');
+            if (CatGame(Game)) GameOver('c');
+        }
+
+        
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label3.Text = "(" + e.X + " , " + e.Y + ")";
+            label2.Text = e.Button.ToString(); // Left or Right
+            // Determine which square click was in
+            int s = 0;
+            if (e.X < X2)
+            {
+                if (e.Y < Y2)
+                    s = 0; // top left square
+                else
+                {
+                    if (e.Y < Y3)
+                        s = 3; // middle left square
+                    else
+                        s = 6; // bottom left square
+                }
+            }
+            else
+            {
+                if (e.X < X3)
+                {
+                    if (e.Y < Y2)
+                        s = 1; // top middle square
+                    else
+                    {
+                        if (e.Y < Y3)
+                            s = 4; // middle square
+                        else
+                            s = 7; // bottom middle square
+                    }
+                }
+                else
+                {
+                    if (e.Y < Y2)
+                        s = 2; // top right square
+                    else
+                    {
+                        if (e.Y < Y3)
+                            s = 5; // middle right square
+                        else
+                            s = 8; // bottom right square
+                    }
+                }
+            }
+            label2.Text = s.ToString();
+            // see if square is open
+            if (Game[s] == '-') // if square is open then move (put X) in that square
+            {
+                Game = Game.Substring(0, s) + "X" + Game.Substring(s + 1);
+                DrawGame(pictureBox1.Width);
+            }
+        } // end method pictureBox1_MouseDown
+
+
+
+        bool WonGame(string GameState, char C)
+        {
+            if ((GameState[0] == C) && (GameState[1] == C) && (GameState[2] == C)) return true; // top row
+            if ((GameState[3] == C) && (GameState[4] == C) && (GameState[5] == C)) return true; // middle row
+            if ((GameState[6] == C) && (GameState[7] == C) && (GameState[8] == C)) return true; // bottom row
+            if ((GameState[0] == C) && (GameState[3] == C) && (GameState[6] == C)) return true; // left column
+            if ((GameState[1] == C) && (GameState[4] == C) && (GameState[7] == C)) return true; // middle column
+            if ((GameState[2] == C) && (GameState[5] == C) && (GameState[8] == C)) return true; // right column
+            if ((GameState[0] == C) && (GameState[4] == C) && (GameState[8] == C)) return true; // main diagonal
+            if ((GameState[2] == C) && (GameState[4] == C) && (GameState[6] == C)) return true; // off diagonal
+            return false;
+        }
+
+        bool CatGame(string GameState)
+        {
+            int n = 0;
+            for (int i = 0; i < 9; i++)
+                if (GameState[i] == '-') n++;
+            if (n > 0) return false;
+            if (WonGame(GameState, 'X')) return false;
+            if (WonGame(GameState, 'O')) return false;
+            return true;
+        }
+
+        void GameOver(char Winner)
+        {
+            string Caption = "Cat game";
+            if (Winner == 'X')
+            {
+                Caption = "Player wins";
+                for (int i = 0; i < 9; i++)
+                {
+                    if (Game[i] == 'X')
+                    {
+                        Library.Add(Game.Substring(0, i) + '-' + Game.Substring(i + 1) + " L");
+                    }
+                }
+            }
+            if (Winner == 'O') Caption = "Computer wins";
+            MessageBox.Show("Game over", Caption);
+            Game = "---------"; // reset game state
+        }
+
+
+
+        bool Better(string New, string Old) // make strict inequality (true iff New > Old)
+        {
+            if ((Old == "L") && (New != "L")) return true;
+            if ((Old == "C") && (New != "C") && (New != "L")) return true;
+            if ((Old == "U") && (New != "L") && (New != "C") && (New != "U")) return true;
+            if (Old == "W") return false;
+            return false;
+        }
+
+
+
+        // Computer move
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Moves = new ArrayList();
+            for (int i=0; i<9; i++)
+            {
+                if (Game[i] == '-') // possible move for '0'
+                {
+                    string Temp = Game.Substring(0, i) + 'O' + Game.Substring(i + 1);
+                    bool Found = false;
+                    foreach (string State in Library)
+                    {
+                        if (Temp == State.Substring(0, 9)) // match found in knowledge database
+                        {
+                            Moves.Add(new Play(i, State.Substring(10, 1)));
+                            Found = true;
+                        }
+                    } // end foreach
+                    if (!Found)
+                    {
+                        Moves.Add(new Play(i, "U"));
+                    }
+                }
+            } // end for
+
+            // Show moves
+            listBox1.Items.Clear();
+            foreach (Play move in Moves)
+                listBox1.Items.Add(move);
+
+            // Check if all plays are not unknown - if so, then add prior moves to library
+            bool AllOutcomesKnown = true;
+            foreach (Play p in Moves)
+            {
+                if (p.Outcome == "U") AllOutcomesKnown = false;
+            }
+            if (AllOutcomesKnown)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (Game[i] == 'X')
+                    {
+                        Library.Add(Game.Substring(0, i) + '-' + Game.Substring(i + 1) + " L");
+                    }
+                }
+            }
+
+            // Choose next "best" move in order
+            Play m = (Play)Moves[0];
+            foreach (Play p in Moves)
+            {
+                if (Better(p.Outcome, m.Outcome)) m = p; // p > m
+            }
+            Game = Game.Substring(0, m.Position) + 'O' + Game.Substring(m.Position + 1);
+            DrawGame(pictureBox1.Width);
+        } // end method button2_Click
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("library.txt");
+            foreach (string s in Library)
+                sw.WriteLine(s);
+            sw.Close();
+        } 
+
+    }
+}
